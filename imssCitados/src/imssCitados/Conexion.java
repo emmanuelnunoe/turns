24cd7f2a852a29f7892600e8bbe5b6e1a231a7e0
emmanuel <emmanuel.nuno.estrella@uabc.edu.mx>
2020-03-29 20:52:54 -0700

    Update funcionamiento
    update funcionamiento

---
+++ imssCitados/src/imssCitados/Conexion.java
@@ -0,0 +1,14 @@
+package imssCitados;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Conexion {
+
+    private final int PUERTO = 1234; //Puerto para la conexion
+    private final String HOST = "localhost"; //Host para la conexion
+
+
+}
