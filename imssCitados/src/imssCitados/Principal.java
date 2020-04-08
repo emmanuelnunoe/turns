6287da1507454c7fb9865bfb78e4981fb6885b16
emmanuelne <39935404+emmanuelnunoe@users.noreply.github.com>
2020-04-07 21:45:44 -0700

    modificacion clase principal: iniciar conteo en 0

--- imssCitados/src/imssCitados/Principal.java
+++ imssCitados/src/imssCitados/Principal.java
@@ -1,11 +1,16 @@
 package imssCitados;
 
+import javax.swing.JLabel;
+
 public class Principal {
 
     public static void main(String[] args) {
     //crar nueva ventana
 
         Turnos turnos = new Turnos();
+        JLabel iniciar = new JLabel();
+        iniciar.setText("0");
+        turnos.setConteoLabel(iniciar);
         
 
     }
